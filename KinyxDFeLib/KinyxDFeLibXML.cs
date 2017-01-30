326a4bed431498a673809df67dab32b4245ace5a
Jeferson <jefersonjr@outlook.com.br>
2017-01-30 01:10:52 -0400

    Adicionado selecao de certificado

--- KinyxDFeLib/KinyxDFeLibXML.cs
+++ KinyxDFeLib/KinyxDFeLibXML.cs
@@ -1,7 +1,9 @@
 ﻿using RGiesecke.DllExport;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
 using System.Text;
@@ -10,6 +12,28 @@ using System.Xml;
 
 namespace KinyxDFeLib
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Certificado
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 50)]
+        public string SerialNumber;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+        public string Subject;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+        public string IssuerName;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 15)]
+        public string CNPJ;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+        public string ExpirationDate;
+
+        [MarshalAs(UnmanagedType.U1)]
+        public bool IsHardwareDevice;
+    }
+
     public class KinyxDFeLibXML
     {
         /// <summary>
@@ -19,7 +43,7 @@ namespace KinyxDFeLib
         /// <param name="serialCertificado">Serial do certificado a ser utilizado</param>
         /// <param name="xmlAssinado">XML assinado</param>
         [DllExport(CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
-        public static void AssinarXML([MarshalAs(UnmanagedType.BStr)] string conteudoXML, [MarshalAs(UnmanagedType.BStr)] string serialCertificado, [MarshalAs(UnmanagedType.BStr)] out string xmlAssinado)
+        public static void AssinarXML([MarshalAs(UnmanagedType.BStr)] string conteudoXML, [MarshalAs(UnmanagedType.BStr)] string serialCertificado, [MarshalAs(UnmanagedType.BStr)] string infElement, [MarshalAs(UnmanagedType.BStr)] out string xmlAssinado)
         {
             try
             {
@@ -30,12 +54,18 @@ namespace KinyxDFeLib
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.LoadXml(conteudoXML);
 
-                // Assinar infNFe
-                XmlElement nodeInfNFe = xmlDoc.GetElementsByTagName("infNFe")[0] as XmlElement;
+                // Obter nó a ser assinado
+                XmlNodeList nodeElementList = xmlDoc.GetElementsByTagName(infElement);
+
+                if (nodeElementList.Count == 0)
+                    throw new Exception(String.Format("Elemento {0} não encontrado no XML informado.", infElement));
+
+                // Obter primeiro nó encontrado
+                XmlElement nodeElement = nodeElementList[0] as XmlElement;
 
                 // ID da tag
-                string id = nodeInfNFe.Attributes.GetNamedItem("Id").Value;
-                SignedXml signer = new SignedXml(nodeInfNFe);
+                string id = nodeElement.Attributes.GetNamedItem("Id").Value;
+                SignedXml signer = new SignedXml(nodeElement);
                 signer.SigningKey = certificado.PrivateKey;
 
                 Reference reference = new Reference("#" + id);
@@ -59,8 +89,86 @@ namespace KinyxDFeLib
             }
             catch (Exception ex)
             {
+                // MSG PARA DEBUG
                 MessageBox.Show(ex.Message);
                 xmlAssinado = "";
+            }
+        }
+
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static void SelecionarCertificado([MarshalAs(UnmanagedType.BStr)] string numeroSerie, IntPtr ptrCertificado)
+        {
+            try
+            {
+                // Certificado selecionado - retornar para aplicação
+                X509Certificate2 certificado = default(X509Certificate2);
+
+                // Abrir seleção de certificado digital
+                X509Certificate2Collection certificados = Utilitarios.CarregarCertificados();
+
+                // Se não informado serial, abrir seleção de certificado
+                if (string.IsNullOrEmpty(numeroSerie))
+                {
+                    X509Certificate2Collection certSelecionado = X509Certificate2UI.SelectFromCollection(certificados, "Certificados", "Selecionar certificado digital", X509SelectionFlag.SingleSelection);
+
+                    if (certSelecionado.Count > 0)
+                        certificado = certSelecionado[0];
+                }
+                else
+                {
+                    // Se informado número de série, procurar diretamente o certificado
+                    X509Certificate2Collection lstCert = certificados.Find(X509FindType.FindBySerialNumber, numeroSerie, false);
+
+                    if (lstCert.Count > 0)
+                        certificado = lstCert[0];
+                }
+
+                // Obter estrutura através do ponteiro informado
+                Certificado cert = (Certificado)Marshal.PtrToStructure(ptrCertificado, typeof(Certificado));
+
+                // Preencher dados do certificado digital
+                cert.SerialNumber = certificado.SerialNumber;
+                cert.Subject = certificado.Subject;
+                cert.ExpirationDate = certificado.GetExpirationDateString();
+                cert.IssuerName = certificado.IssuerName.Format(false);
+                cert.IsHardwareDevice = false;
+
+                if(certificado.HasPrivateKey)
+                {
+                    AsymmetricAlgorithm privKey = certificado.PrivateKey;
+                    RSACryptoServiceProvider provider = (privKey as RSACryptoServiceProvider);
+
+                    if (provider != null)
+                        cert.IsHardwareDevice = provider.CspKeyContainerInfo.HardwareDevice;
+                }
+
+                // Obter CNPJ das propriedades
+                foreach (X509Extension ext in certificado.Extensions)
+                {
+                    string propriedades = ext.Format(true);
+
+                    if (propriedades.IndexOf("2.16.76.1.3.3") >= 0)
+                    {
+                        string[] propLinhas = propriedades.Split('\n');
+
+                        foreach (string linha in propLinhas)
+                        {
+                            if (linha.IndexOf("2.16.76.1.3.3") >= 0)
+                            {
+                                int posicaoSeparador = linha.IndexOf("=");
+                                cert.CNPJ = Utilitarios.ConverterHexToAscii(linha.Substring(posicaoSeparador + 1, linha.Length - posicaoSeparador - 2));
+                            }
+                        }
+                    }
+                }
+
+                // Realocar estrutura alterada para o ponteiro
+                Marshal.StructureToPtr(cert, ptrCertificado, false);
+            }
+            catch (Exception ex)
+            {
+                //DEBUG
+                MessageBox.Show(ex.Message);
             }
         }
     }
--- KinyxDFeLib/Utilitarios.cs
+++ KinyxDFeLib/Utilitarios.cs
@@ -7,16 +7,41 @@ namespace KinyxDFeLib
 {
     public static class Utilitarios
     {
-        public static X509Certificate2 GetCertificadoPorSerial(string numeroSerial)
+        public static X509Certificate2Collection CarregarCertificados()
         {
             X509Store certStore = new X509Store("MY", StoreLocation.CurrentUser);
             certStore.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
-            X509Certificate2Collection findCert = certStore.Certificates.Find(X509FindType.FindBySerialNumber, numeroSerial, false);
+            return certStore.Certificates;
+        }
+
+        public static X509Certificate2 GetCertificadoPorSerial(string numeroSerial)
+        {
+            X509Certificate2Collection findCert = CarregarCertificados().Find(X509FindType.FindBySerialNumber, numeroSerial, false);
 
             if (findCert.Count > 0)
                 return findCert[0];
             else
                 throw new Exception(String.Format("Certificado com o serial '{0}' não encontrado.", numeroSerial));
+        }
+
+        public static string ConverterHexToAscii(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+
+            string asciiString = "";
+            for (int i = 0; i < hexString.Length; i += 2)
+            {
+                if (hexString.Length >= i + 2)
+                {
+                    String hs = hexString.Substring(i, 2);
+                    char tmpChar = System.Convert.ToChar(System.Convert.ToUInt32(hexString.Substring(i, 2), 16));
+
+                    if (char.IsDigit(tmpChar))
+                        asciiString += tmpChar.ToString();
+                }
+            }
+
+            return asciiString;
         }
     }
 }
